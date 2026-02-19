d2bba5b970721f2dd4eaad748929b2e5bdf9ac68
Brad Student <bradley.reddish@student.swic.edu>
2026-02-19 14:51:54 -0600

    Add personal information variables

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun (plain JS)!");
+// Personal Information
+const firstName = "Bradley";
+const lastName = "Reddish";
+const age = 26; // Your actual age
+const isStudent = true;
+
+// Using template literals
+const introduction = `Hello! My name is $Bradley $Reddish. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+// Print to console
+console.log(introduction);
+console.log(studentStatus);
